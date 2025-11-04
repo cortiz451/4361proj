291b85073e5750c3320c0bc736ae361def052108
Carlos <cxo210012@utdallas.edu>
2025-11-04 15:15:45 -0600

    Coins 2

--- objects/coin.gd
+++ objects/coin.gd
@@ -3,5 +3,7 @@ extends Area3D
 #add a coin congrats!
 func _on_body_entered(body: Node3D) -> void:
 	if body.has_method("coin_get"):
+		Audio.play("sounds/coin.ogg")
 		body.coin_get()
+		queue_free()
 	
