0c119e4ee948c66da7469f6cd4722fda0c2c3b8f
Carlos <cxo210012@utdallas.edu>
2025-11-04 20:08:13 -0600

    UI updates (face 😄) + more textures for later
    Going with the paper aesthetic for now

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,6 +4,19 @@ var displayAmmo = true
 
 func _on_health_updated(health):
 	$Health.text = "Health: "+ str(health) + "%"
+	
+	if(health>100):
+		$face_wow.visible=true
+	elif(health>50):
+		$face_wow.visible=false
+		$face_good.visible=true
+	elif(health>25):
+		$face_good.visible=false
+		$face_ok.visible=true
+	else:
+		$face_ok.visible=false
+		$face_bad.visible=true
+	
 
 func _on_player_ammo_updated(weaponammo, type) -> void:
 	if(displayAmmo):
